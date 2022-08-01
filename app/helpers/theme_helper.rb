3b63a9d0ba48a6a4e07effce19e83cfa219687cf
Kasper Timm Hansen <kaspth@gmail.com>
2022-08-01 21:26:21 +0200

    Convert call structure check to a guard clause
    Doing `return super unless options.is_a?(String)` lets us check out of
    our method body early, and thus omit the later check in the `rescue` too.
    
    This also lets us consolidate the call structure comment to the top of
    the method body.

--- app/helpers/theme_helper.rb
+++ app/helpers/theme_helper.rb
@@ -14,60 +14,54 @@ module ThemeHelper
 
   def render(options = {}, locals = {}, &block)
     # The theme engine only supports `<%= render 'shared/box' ... %>` style calls to `render`.
-    if options.is_a?(String)
-      # TODO This caching should be disabled in development so new templates are taken into account without restarting the server.
-      options = BulletTrain::Themes.partial_paths[options] || options
-    end
+    return super unless options.is_a?(String)
 
-    begin
-      # This is where we try to just lean on Rails default behavior. If someone renders `shared/box` and also has a
-      # `app/views/shared/_box.html.erb`, then no error will be thrown and we will have never interfered in the normal
-      # Rails behavior.
-      #
-      # We also don't do anything special if someone renders `shared/box` and we've already previously resolved that
-      # partial to be served from `themes/light/box`. In that case, we've already replaced `shared/box` with the
-      # actual path of the partial, and Rails will do the right thing from this point.
-      #
-      # However, if one of those two situations isn't true, then this call here will throw an exception and we can
-      # perform the appropriate magic to figure out where amongst the themes the partial should be rendering from.
-      super
-    rescue ActionView::MissingTemplate => exception
-      # The theme engine only supports `<%= render 'shared/box' ... %>` style calls to `render`.
-      if options.is_a?(String)
+    # TODO This caching should be disabled in development so new templates are taken into account without restarting the server.
+    options = BulletTrain::Themes.partial_paths[options] || options
 
-        # Does the requested partial path match one of the invocation regexes?
-        if (invocation_pattern = INVOCATION_PATTERNS.detect { |regex| options.match?(regex) })
-          # Keep track of the original options.
-          original_options = options
+    # This is where we try to just lean on Rails default behavior. If someone renders `shared/box` and also has a
+    # `app/views/shared/_box.html.erb`, then no error will be thrown and we will have never interfered in the normal
+    # Rails behavior.
+    #
+    # We also don't do anything special if someone renders `shared/box` and we've already previously resolved that
+    # partial to be served from `themes/light/box`. In that case, we've already replaced `shared/box` with the
+    # actual path of the partial, and Rails will do the right thing from this point.
+    #
+    # However, if one of those two situations isn't true, then this call here will throw an exception and we can
+    # perform the appropriate magic to figure out where amongst the themes the partial should be rendering from.
+    super
+  rescue ActionView::MissingTemplate => exception
+    # Does the requested partial path match one of the invocation regexes?
+    if (invocation_pattern = INVOCATION_PATTERNS.detect { |regex| options.match?(regex) })
+      # Keep track of the original options.
+      original_options = options
 
-          # Trim out the base part of the requested partial.
-          requested_partial = options.gsub(invocation_pattern, "")
+      # Trim out the base part of the requested partial.
+      requested_partial = options.gsub(invocation_pattern, "")
 
-          # TODO We're hard-coding this for now, but this should probably come from the `Current` model.
-          current_theme_object.directory_order.each do |theme_path|
-            # Update our options from something like `shared/box` to `themes/light/box`.
-            options = "themes/#{theme_path}/#{requested_partial}"
+      # TODO We're hard-coding this for now, but this should probably come from the `Current` model.
+      current_theme_object.directory_order.each do |theme_path|
+        # Update our options from something like `shared/box` to `themes/light/box`.
+        options = "themes/#{theme_path}/#{requested_partial}"
 
-            # Try rendering the partial again with the updated options.
-            body = super
+        # Try rendering the partial again with the updated options.
+        body = super
 
-            # 🏆 If we get this far, then we've found the actual path of the theme partial. We should cache it!
-            BulletTrain::Themes.partial_paths[original_options] = options
+        # 🏆 If we get this far, then we've found the actual path of the theme partial. We should cache it!
+        BulletTrain::Themes.partial_paths[original_options] = options
 
-            # We also need to return whatever the rendered body was.
-            return body
+        # We also need to return whatever the rendered body was.
+        return body
 
-          # If calling `render` with the updated options is still resulting in a missing template, we need to
-          # keep iterating over `directory_order` to work our way up the theme stack and see if we can find the
-          # partial there, e.g. going from `light` to `tailwind` to `base`.
-          rescue ActionView::MissingTemplate => _
-            next
-          end
-        end
+      # If calling `render` with the updated options is still resulting in a missing template, we need to
+      # keep iterating over `directory_order` to work our way up the theme stack and see if we can find the
+      # partial there, e.g. going from `light` to `tailwind` to `base`.
+      rescue ActionView::MissingTemplate => _
+        next
       end
-
-      # If we weren't able to find the partial in some theme-based place, then just let the original error bubble up.
-      raise exception
     end
+
+    # If we weren't able to find the partial in some theme-based place, then just let the original error bubble up.
+    raise exception
   end
 end
